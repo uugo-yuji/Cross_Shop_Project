24d8c02aae65795bf69d870af195e8994edb84fa
uugo-yuji <uugo.hosokai@gmail.com>
2020-02-07 13:25:27 +0900

    エラーのため一回コメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   #deivse
-  config.action_mailer.default_url_options = { https://cross-shop-bbs.herokuapp.com }
+  #config.action_mailer.default_url_options = { https://cross-shop-bbs.herokuapp.com }
 end
